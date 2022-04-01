f2d028917899bff2b85bf3c4d089c2a57f03d7f4
Rashed Hasan <rashed50702@gmail.com>
2022-04-01 18:14:49 +0600

    product crud

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 class ProductsController extends Controller
 {
@@ -35,7 +37,30 @@ class ProductsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $filename = Null;
+
+            $this->validate($request, [
+                'name' => 'required|max:255',
+                'price' => 'required|numeric',
+            ]);
+
+            if ($request->file('image')) {
+
+                $this->validate($request, ['image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',]);
+                $filename = time().'.'.$request->file('image')->getClientOriginalExtension();
+                $request->image->move(public_path('assets/images/products'), $filename);
+            }
+
+            $product = new Product();
+            $product->name  = $request->name;
+            $product->price = $request->price;
+            $product->image = $filename;
+            $product->save();
+            return response()->json(['status' => 'success', 'message' => 'Product successfully saved!'], 201);
+        }catch (\Exception $exception) {
+            return response()->json(['status' => 'error', 'message' => 'Error', 'errors' => $exception->errors()], 422);
+        }
     }
 
     /**
@@ -46,7 +71,7 @@ class ProductsController extends Controller
      */
     public function show($id)
     {
-        //
+        return Product::where('id', $id)->first();
     }
 
     /**
@@ -69,7 +94,39 @@ class ProductsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            // return $id;
+            // return $request->all();
+            $this->validate($request, [
+                'name' => 'required|max:255',
+                'price' => 'required',
+            ]);
+
+            $product = Product::find($id);
+            $product->name  = $request->name;
+            $product->price = $request->price;
+            if ($request->file('image')) {
+
+                $this->validate($request, ['image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',]);
+
+                //remove old photo from folder if exist
+                if ($product->image) {
+                    $productImage = public_path("assets/images/products/{$product->image}");
+                    if (File::exists($productImage)) {
+                        unlink($productImage);
+                    }
+                }
+
+
+                $filename = time().'.'.$request->file('image')->getClientOriginalExtension();
+                $request->image->move(public_path('assets/images/products'), $filename);
+                $product->image = $filename;
+            }
+            $product->save();            
+            return response()->json(['status' => 'success', 'message' => 'Product successfully updated!'], 201);
+        }catch (\Exception $exception) {
+            return response()->json(['status' => 'error', 'message' => 'Error', 'errors' => $exception->errors()], 422);
+        }
     }
 
     /**
@@ -80,6 +137,15 @@ class ProductsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product = Product::find($id);
+        if ($product->image) {
+            $productImage = public_path("assets/images/products/{$product->image}");
+            if (File::exists($productImage)) {
+                unlink($productImage);
+            }
+        }
+        $product->delete();
+
+        return Response::json(['message' => 'The product deleted successfully!'], 200);
     }
 }
